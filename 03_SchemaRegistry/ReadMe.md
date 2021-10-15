0488f7d0bb79dc5678772e35695e4f60f85f71e6
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2021-10-15 18:58:20 +0100

    added schemas to schema registry

--- 03_SchemaRegistry/ReadMe.md
+++ 03_SchemaRegistry/ReadMe.md
@@ -1,5 +1,8 @@
 Ensure PATH in system variables has path to python!
 
+Forwards compatibility for the win
+[Compatibility](https://stevenheidel.medium.com/backward-vs-forward-compatibility-9c03c3db15c9#:~:text=Backward%20compatibility%20means%20that%20readers,writers%20with%20a%20newer%20schema.)
+
 ```
 python -m venv venv
 ```
@@ -10,6 +13,7 @@ venv\scripts\activate
 
 ```
 pip install azure-schemaregistry-avroserializer azure-identity
+pip install azure-schemaregistry azure-identity
 ```
 
 Create app reg aprg-events-admin
--- 03_SchemaRegistry/schemaregister.py
+++ 03_SchemaRegistry/schemaregister.py
@@ -1,33 +1,78 @@
-import os
 from azure.schemaregistry import SchemaRegistryClient
-from azure.schemaregistry.serializer.avroserializer import AvroSerializer
-from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.identity import DefaultAzureCredential
 
-credential = DefaultAzureCredential()
-
-# token_credential = ClientSecretCredential(
-#     tenant_id=TENANT_ID,
-#     client_id=CLIENT_ID,
-#     client_secret=CLIENT_SECRET
-# )
-
-
+token_credential = DefaultAzureCredential()
+# Namespace should be similar to: '<your-eventhub-namespace>.servicebus.windows.net/'
 fully_qualified_namespace = 'schemaregistry-ehns-eun-griff.servicebus.windows.net'
-group_name = 'schema_registry'
-schema_registry_client = SchemaRegistryClient(fully_qualified_namespace, credential)
-serializer = AvroSerializer(client=schema_registry_client, group_name=group_name)
+schema_registry_client = SchemaRegistryClient(fully_qualified_namespace, token_credential)
 
-schema_string = """
+group_name = "schema_registry"
+format = "Avro"
+
+customer_name = "customer_created"
+customer_schema_definition = """
 {"namespace": "example.avro",
  "type": "record",
- "name": "User",
+ "name": "Customer.Created",
  "fields": [
-     {"name": "name", "type": "string"},
-     {"name": "favorite_number",  "type": ["int", "null"]},
-     {"name": "favorite_color", "type": ["string", "null"]}
+     {"name": "customer_guid", "type": "string"},
+     {"name": "created_date", "type": "string"},
+     {"name": "email", "type": "string"}
  ]
-}"""
+}
+"""
 
-with serializer:
-    dict_data = {"name": "Ben", "favorite_number": 7, "favorite_color": "red"}
-    encoded_bytes = serializer.serialize(dict_data, schema=schema_string)
+lead_name = "lead_created"
+lead_schema_definition = """
+{"namespace": "example.avro",
+ "type": "record",
+ "name": "Lead.Created",
+ "fields": [
+     {"name": "lead_guid", "type": "string"},
+     {"name": "lead_date", "type": "string"},
+     {"name": "customer_guid", "type": "string"},
+     {"name": "product_id", "type": "int"}
+ ]
+}
+"""
+
+sale_name = "sale_created"
+sale_schema_definition = """
+{"namespace": "example.avro",
+ "type": "record",
+ "name": "Sale.Created",
+ "fields": [
+     {"name": "sale_guid", "type": "string"},
+     {"name": "sale_date", "type": "string"},
+     {"name": "lead_guid", "type": "string"}
+ ]
+}
+"""
+
+sale_confirmed_name = "sale_confirmed"
+sale_confirmed_schema_definition = """
+{"namespace": "example.avro",
+ "type": "record",
+ "name": "Sale.Confirmed",
+ "fields": [
+     {"name": "sale_guid", "type": "string"},
+     {"name": "sale_date", "type": "string"},
+     {"name": "lead_guid", "type": "string"},
+     {"name": "customer_guid", "type": "string"},
+     {"name": "product_id", "type": "int"}
+ ]
+}
+"""
+
+schemas = {
+     customer_name: customer_schema_definition,
+    lead_name: lead_schema_definition, 
+    sale_confirmed_name: sale_confirmed_schema_definition, 
+    sale_name: sale_schema_definition} 
+
+schema_registry_client = SchemaRegistryClient(fully_qualified_namespace=fully_qualified_namespace, credential=token_credential)
+with schema_registry_client:
+    for k, v in schemas.items():
+        print(k, v)
+        schema_properties = schema_registry_client.register_schema(group_name, k, v, format)
+        id = schema_properties.id
